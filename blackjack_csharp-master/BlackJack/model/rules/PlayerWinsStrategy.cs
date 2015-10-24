b52ebb6488275ce02717bcb91c9382e580c09883
ss223ck <ss223ck@student.lnu.se>
2015-10-24 19:24:08 +0200

    updaterat classdiagram

---
+++ blackjack_csharp-master/BlackJack/model/rules/PlayerWinsStrategy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlackJack.model.rules
+{
+    class PlayerWinsStrategy : IWhoWinsRule
+    {
+        public bool IsPlayerWinner(Player player, Player dealer, int maxScore)
+        {
+            if(player.CalcScore() > maxScore)
+            {
+                return false;
+            } else if(dealer.CalcScore() > maxScore)
+            {
+                return true;
+            }
+            return dealer.CalcScore() <= player.CalcScore();
+        }
+    }
+}
